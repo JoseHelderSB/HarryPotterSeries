3f9e22b9d37f915a391f9b0f6a643a869bb96019
José Helder <joseheldersb@gmail.com>
2022-04-03 22:45:56 -0300

    background color

--- main.js
+++ main.js
@@ -1,111 +1,19 @@
-//BOOK 1
-var book = document.querySelector("#book1");
-console.log(book);
+var books = document.querySelectorAll(".books");
+console.log(books);
 
-var tdBookYear = book.querySelector(".bookYear");
-var bookYear = tdBookYear.textContent;
+for (let i = 0; i < books.length; i++) {
 
-var tdMovieYear = book.querySelector(".movieYear");
-var movieYear = tdMovieYear.textContent;
+    book = books[i];
 
-var tdGap = book.querySelector(".gap");
+    var tdBookYear = book.querySelector(".bookYear");
+    var bookYear = tdBookYear.textContent;
 
-var gap = movieYear - bookYear;
+    var tdMovieYear = book.querySelector(".movieYear");
+    var movieYear = tdMovieYear.textContent;
 
-tdGap.textContent = `${gap} years`;
+    var tdGap = book.querySelector(".gap");
 
-//BOOK 2
-var book = document.querySelector("#book2");
-console.log(book);
+    var gap = movieYear - bookYear;
 
-var tdBookYear = book.querySelector(".bookYear");
-var bookYear = tdBookYear.textContent;
-
-var tdMovieYear = book.querySelector(".movieYear");
-var movieYear = tdMovieYear.textContent;
-
-var tdGap = book.querySelector(".gap");
-
-var gap = movieYear - bookYear;
-
-tdGap.textContent = `${gap} years`;
-
-//BOOK 3
-var book = document.querySelector("#book3");
-console.log(book);
-
-var tdBookYear = book.querySelector(".bookYear");
-var bookYear = tdBookYear.textContent;
-
-var tdMovieYear = book.querySelector(".movieYear");
-var movieYear = tdMovieYear.textContent;
-
-var tdGap = book.querySelector(".gap");
-
-var gap = movieYear - bookYear;
-
-tdGap.textContent = `${gap} years`;
-
-//BOOK 4
-var book = document.querySelector("#book4");
-console.log(book);
-
-var tdBookYear = book.querySelector(".bookYear");
-var bookYear = tdBookYear.textContent;
-
-var tdMovieYear = book.querySelector(".movieYear");
-var movieYear = tdMovieYear.textContent;
-
-var tdGap = book.querySelector(".gap");
-
-var gap = movieYear - bookYear;
-
-tdGap.textContent = `${gap} years`;
-
-//BOOK 5
-var book = document.querySelector("#book5");
-console.log(book);
-
-var tdBookYear = book.querySelector(".bookYear");
-var bookYear = tdBookYear.textContent;
-
-var tdMovieYear = book.querySelector(".movieYear");
-var movieYear = tdMovieYear.textContent;
-
-var tdGap = book.querySelector(".gap");
-
-var gap = movieYear - bookYear;
-
-tdGap.textContent = `${gap} years`;
-
-//BOOK 6
-var book = document.querySelector("#book6");
-console.log(book);
-
-var tdBookYear = book.querySelector(".bookYear");
-var bookYear = tdBookYear.textContent;
-
-var tdMovieYear = book.querySelector(".movieYear");
-var movieYear = tdMovieYear.textContent;
-
-var tdGap = book.querySelector(".gap");
-
-var gap = movieYear - bookYear;
-
-tdGap.textContent = `${gap} years`;
-
-//BOOK 7
-var book = document.querySelector("#book7");
-console.log(book);
-
-var tdBookYear = book.querySelector(".bookYear");
-var bookYear = tdBookYear.textContent;
-
-var tdMovieYear = book.querySelector(".movieYear");
-var movieYear = tdMovieYear.textContent;
-
-var tdGap = book.querySelector(".gap");
-
-var gap = movieYear - bookYear;
-
-tdGap.textContent = `${gap} years`;
+    tdGap.textContent = `${gap} years`;
+}
